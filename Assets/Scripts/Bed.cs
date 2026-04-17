f5e1fad38ca19263251f3a03018999c22ce836b4
Angela Garcia <100092195+angelargd8@users.noreply.github.com>
2026-04-17 17:43:38 -0600

    chore: light condition

--- Assets/Scripts/Bed.cs
+++ Assets/Scripts/Bed.cs
@@ -1,20 +1,29 @@
 using UnityEngine;
+using static UnityEngine.InputSystem.Controls.AxisControl;
 
 public class Bed : MonoBehaviour, IInteractable
 {
     [SerializeField] private Transform sleepPoint;
     [SerializeField] private SleepSystem sleepSystem;
 
+    [SerializeField] private Lamp lamp;
 
     public string GetInteractionText()
     {
+        if (lamp != null && lamp.isOn)
+            return "Apaga la luz para dormir";
+
+
         return "Presiona E para dormir";
     }
 
     public void Interact()
     {
-        Debug.Log("Durmiendo");
-        
+        if (lamp != null && lamp.isOn)
+        {
+            Debug.Log("No puedes dormir con la luz encendida.");
+            return;
+        }
 
         if (sleepSystem != null && sleepPoint != null)
         {
