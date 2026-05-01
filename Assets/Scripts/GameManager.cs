956eb9826cef3fd358c918e9c72132e532e766ee
Angela Garcia <100092195+angelargd8@users.noreply.github.com>
2026-05-01 00:01:21 -0600

    chore: load and unload loading screen

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -30,16 +30,35 @@ public class GameManager : MonoBehaviour
         StartCoroutine(LoadSceneAsync(sceneName));
     }
 
+
     IEnumerator LoadSceneAsync(string sceneName)
     {
+                
         var operation = SceneManager.LoadSceneAsync(sceneName);
+        operation.allowSceneActivation = false;
+
+
+        //TODO: aqui luego editar otras ciertas escenas y manejar mejor el redirijir a otras escenas
+
+        SceneManager.LoadScene("LoadingScene", LoadSceneMode.Additive);
+
+        while (operation.progress < 0.9)
+        {
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(3.0f);
+
+        SceneManager.UnloadSceneAsync("LoadingScene");
+        operation.allowSceneActivation = true;
+
 
         while (!operation.isDone)
         {
             yield return null;
         }
 
-        
+
     }
 
 
